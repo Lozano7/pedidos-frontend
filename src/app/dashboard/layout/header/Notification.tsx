270d0a3422aaaf509fab162476517ba7023afaa3
alozano-links <56984985+Lozano7@users.noreply.github.com>
2024-02-13 13:26:04 -0500

    fix notificacion

--- src/app/dashboard/layout/header/Notification.tsx
+++ src/app/dashboard/layout/header/Notification.tsx
@@ -1,16 +1,51 @@
 'use client';
+import { baseURL } from '@/api/mainApi';
+import { Pedido } from '@/store/features/report/interfaces/consumption.interface';
 import { Badge, Box, IconButton, Menu } from '@mui/material';
 import { IconBellRinging } from '@tabler/icons-react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 
 const Notification = () => {
   const [anchorEl2, setAnchorEl2] = useState(null);
+  const [notifications, setNotifications] = useState<Pedido[]>([]);
+
+  console.log(notifications);
+
   const handleClick2 = (event: any) => {
     setAnchorEl2(event.currentTarget);
   };
   const handleClose2 = () => {
     setAnchorEl2(null);
   };
+
+  useEffect(() => {
+    const socket = io(baseURL);
+    // Escucha el evento 'newPedido' del servidor y actualiza las notificaciones
+    socket.on('newPedido', (pedido) => {
+      setNotifications((prevNotifications) => [...prevNotifications, pedido]);
+    });
+
+    // Devuelve una función de limpieza para desconectar el socket cuando el componente se desmonta
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    const socket = io();
+
+    // Escucha el evento 'newPedido' del servidor y actualiza las notificaciones
+    socket.on('newPedido', (pedido) => {
+      setNotifications((prevNotifications) => [...prevNotifications, pedido]);
+    });
+
+    // Devuelve una función de limpieza para desconectar el socket cuando el componente se desmonta
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
   return (
     <Box>
       <IconButton
